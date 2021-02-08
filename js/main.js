4f5b3e8951af11eb1289f069af47d344de6cddf0
Geoffroy Blomme <jeanbenkyou@gmail.com>
2021-02-08 14:11:00 +0100

    back-to-top scrolls to the top

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
 let modal;
 let hamburgerMenuImg;
+let backToTop;
 const setup = () => {
     modal = document.querySelector(".modal");
     hamburgerMenuImg = document.querySelector(".main-header__nav__hamburger-menu img");
     modal.addEventListener("click",hideModal);
     hamburgerMenuImg.addEventListener("click",showModal);
+    backToTop = document.querySelector(".back-to-top");
+    backToTop.addEventListener("click",topFunction);
 }
 
 const showModal = () => {
     modal.style.display = "flex";
     document.querySelector("body").style.overflow = "hidden";
 }
-
+const topFunction = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 const hideModal = () => {
     modal.style.display = "none";
     document.querySelector("body").style.overflow = "visible";
