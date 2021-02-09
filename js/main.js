b7b70a9e44b51312e5559139b6a36a2f93362e5d
Geoffroy Blomme <jeanbenkyou@gmail.com>
2021-02-09 16:08:19 +0100

    The back-to-top now scrolls back smoothly

--- js/main.js
+++ js/main.js
@@ -6,18 +6,14 @@ const setup = () => {
     hamburgerMenuImg = document.querySelector(".main-header__nav__hamburger-menu img");
     modal.addEventListener("click",hideModal);
     hamburgerMenuImg.addEventListener("click",showModal);
-    backToTop = document.querySelector(".back-to-top");
-    backToTop.addEventListener("click",topFunction);
+
 }
 
 const showModal = () => {
     modal.style.display = "flex";
     document.querySelector("body").style.overflow = "hidden";
 }
-const topFunction = () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+
 const hideModal = () => {
     modal.style.display = "none";
     document.querySelector("body").style.overflow = "visible";
